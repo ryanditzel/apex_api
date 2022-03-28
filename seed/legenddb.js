cb40c5c937b03b61efb23889e61af4a6bc9c41b5
ryan.ditzel <ryand370@gmail.com>
2022-03-28 16:04:57 -0400

    added some of the characters to legenddb

---
+++ seed/legenddb.js
@@ -0,0 +1,42 @@
+const db = require('../db')
+const { Ability, Legend } = require('..models')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const ash = await new Legend ({
+        name: 'Ash',
+        description: `Born in the unforgiving Frontier, Dr. Ashleigh Reid - still very much a human - learned early that the only person who was going to look out for her was herself. One day, she was hired by a mercenary group for a delicate job: steal an experimental fuel source being researched in the Outlands city of Olympus. It was the perfect job for Reid, who infiltrated and psychologically manipulated the researchers for years. When the lab self-destructed, she died.
+
+        And didn’t.
+        
+        Her brain was put in a simulacrum shell, but in the process, she lost years of memories, and the trauma of her “death” caused her personality to fracture. Ash is the embodiment of the cold, perfectionistic, power-hungry side of Dr. Reid. But behind that confident facade, something sinister is simmering. After an encounter with Horizon that overrides Ash’s simulacrum programming, the framework that kept the vulnerable--yet intelligent--Leigh personality dormant has begun to crack. Ash joins the Games to prove she has transcended all humanity. Leigh’s determined to prove her wrong.`,
+        tag_line: 'Incisive Instigator',
+        img: 'https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/ash/apex-section-bg-legends-ash-xl.jpg.adapt.320w.jpg',
+    })
+    ash.save();
+
+    const bangalore = await new Legend({
+        name: `Bangalore`,
+        description: `Born into a military family where she, her parents, and her four older brothers all served with the IMC, Bangalore has been an exceptional soldier her whole life. Dedicated to the IMC cause, she was top of her class at the IMC Military Academy and the only cadet who could take apart a Peacekeeper, equip it with a Precision Choke hop-up, and put it back together in under twenty seconds – blindfolded.
+
+        In the months following the Battle at Gridiron, the IMS Hestia, with Anita and brother Jackson on board, was attacked by unknown assailants. A bomb on the outer hull blew out a chunk of the ship… but not before Jackson shielded her, sacrificing his own life to save hers. Anita watched helplessly, as Jackson was sucked out into the vacuum of space. After crashing on a Syndicate planet, Anita looked for both work, and the brother she refused to believe had died, so they could begin the decades-long trip back home. Now, she fights to raise money in the Apex Games, in the hopes of finding a pilot willing to take the epic trip back home to reunite with what remains of her family.`,
+        tag_line: `Professional Soldier`,
+        img: `https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-bangalore-xl.jpg.adapt.320w.jpg`
+    })
+    bangalore.save();
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
